234a44cbb9fa9beff7b42e007c54a0cdeaefbb30
tdavis81 <35125581+tdavis81@users.noreply.github.com>
2018-06-08 22:59:52 -0400

    Add files via upload

---
+++ Tictactoe/tictactoe.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.Button6 = New System.Windows.Forms.Button
+        Me.Button7 = New System.Windows.Forms.Button
+        Me.Button8 = New System.Windows.Forms.Button
+        Me.Button9 = New System.Windows.Forms.Button
+        Me.Button10 = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.TextBox_red = New System.Windows.Forms.TextBox
+        Me.TextBox_Green = New System.Windows.Forms.TextBox
+        Me.TextBox_blue = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Button11 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(4, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(59, 54)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(69, 57)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(59, 54)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(134, 57)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(59, 54)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(4, 117)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(59, 54)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(69, 117)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(59, 54)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(134, 117)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(59, 54)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(4, 177)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(59, 54)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(69, 177)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(59, 54)
+        Me.Button8.TabIndex = 7
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(134, 177)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(59, 54)
+        Me.Button9.TabIndex = 8
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(4, 1)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(93, 22)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Restart"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 305)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Red"
+        '
+        'TextBox_red
+        '
+        Me.TextBox_red.Location = New System.Drawing.Point(4, 321)
+        Me.TextBox_red.Name = "TextBox_red"
+        Me.TextBox_red.Size = New System.Drawing.Size(47, 20)
+        Me.TextBox_red.TabIndex = 11
+        Me.TextBox_red.Text = "00"
+        '
+        'TextBox_Green
+        '
+        Me.TextBox_Green.Location = New System.Drawing.Point(69, 321)
+        Me.TextBox_Green.Name = "TextBox_Green"
+        Me.TextBox_Green.Size = New System.Drawing.Size(47, 20)
+        Me.TextBox_Green.TabIndex = 12
+        Me.TextBox_Green.Text = "FF"
+        '
+        'TextBox_blue
+        '
+        Me.TextBox_blue.Location = New System.Drawing.Point(134, 321)
+        Me.TextBox_blue.Name = "TextBox_blue"
+        Me.TextBox_blue.Size = New System.Drawing.Size(47, 20)
+        Me.TextBox_blue.TabIndex = 13
+        Me.TextBox_blue.Text = "00"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(180, 170)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 14
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(79, 305)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 13)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "Green"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(142, 305)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(28, 13)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Blue"
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(206, 318)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(98, 23)
+        Me.Button11.TabIndex = 17
+        Me.Button11.Text = "click"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(386, 353)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox_blue)
+        Me.Controls.Add(Me.TextBox_Green)
+        Me.Controls.Add(Me.TextBox_red)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Tic Tac Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox_red As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox_Green As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox_blue As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+
+End Class
---
+++ Tictactoe/tictactoe.vb
@@ -0,0 +1,253 @@
+﻿Public Class Form1
+    Dim flag As Boolean = False
+    Private Sub Win()
+        If button1.text = "X" And Button2.text = "X" And Button3.text = "X" Then
+            MsgBox("Congrats you win, player x, you wanna cookie ", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button1.Text = "X" And Button4.Text = "X" And Button7.Text = "X" Then
+            MsgBox("Congrats you win, player x you wanna cookie", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button1.Text = "X" And Button5.Text = "X" And Button9.Text = "X" Then
+            MsgBox("Congrats you win, player x you wanna cookie", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button2.Text = "X" And Button5.Text = "X" And Button8.Text = "X" Then
+            MsgBox("Congrats you win, player x you wanna cookie", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button3.Text = "X" And Button6.Text = "X" And Button9.Text = "X" Then
+            MsgBox("Congrats you win, player x you wanna cookie", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button3.Text = "X" And Button5.Text = "X" And Button7.Text = "X" Then
+            MsgBox("Congrats you win, player x you wanna cookie", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button4.Text = "X" And Button5.Text = "X" And Button6.Text = "X" Then
+            MsgBox("Congrats you win, player x you wanna cookie", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button7.Text = "X" And Button8.Text = "X" And Button9.Text = "X" Then
+            MsgBox("Congrats you win, player x you wanna cookie", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button1.Text = "O" And Button2.Text = "O" And Button3.Text = "O" Then
+            MsgBox("Congrats you win, player O you wanna oreo", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button1.Text = "O" And Button4.Text = "O" And Button7.Text = "O" Then
+            MsgBox("Congrats you win, player O you wanna oreo", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button1.Text = "O" And Button5.Text = "O" And Button9.Text = "O" Then
+            MsgBox("Congrats you win, player O you wanna oreo", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button2.Text = "O" And Button5.Text = "O" And Button8.Text = "O" Then
+            MsgBox("Congrats you win, player O you wanna oreo", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button3.Text = "O" And Button6.Text = "O" And Button9.Text = "O" Then
+            MsgBox("Congrats you win, player O you wanna oreo", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button3.Text = "O" And Button5.Text = "O" And Button7.Text = "O" Then
+            MsgBox("Congrats you win, player O you wanna oreo", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button4.Text = "O" And Button5.Text = "O" And Button6.Text = "O" Then
+            MsgBox("Congrats you win, player O you wanna oreo", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+        If Button7.Text = "O" And Button8.Text = "O" And Button9.Text = "O" Then
+            MsgBox("Congrats you win, player O you wanna oreo", MsgBoxStyle.Information)
+            Button1.Text = ""
+            Button2.Text = ""
+            Button3.Text = ""
+            Button4.Text = ""
+            Button5.Text = ""
+            Button6.Text = ""
+            Button7.Text = ""
+            Button8.Text = ""
+            Button9.Text = ""
+            Console.Beep(400, 250)
+            Console.Beep(800, 250)
+        End If
+    End Sub
+    Private Sub Buttons(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click
+        If flag = False Then
+            DirectCast(sender, Button).Text = "X"
+            flag = True
+        Else
+            DirectCast(sender, Button).Text = "O"
+            flag = False
+            Call Win()
+        End If
+        Win()
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+
+
+    End Sub
+
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        Dim hexColor As String = "#" & TextBox_Red.Text & TextBox_Green.Text & TextBox_Blue.Text
+        Me.BackColor = ColorTranslator.FromHtml(hexColor)
+    End Sub
+End Class
+
+
